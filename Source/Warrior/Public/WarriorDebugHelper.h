c55479e716b1ce60c6dde0c7682c60554264c8d5
juice-artur <asasin.lol10@gmail.com>
2026-01-02 18:33:40 +0200

    Setup character camera

---
+++ Source/Warrior/Public/WarriorDebugHelper.h
@@ -0,0 +1,17 @@
+// Warrior, Copyright 2026 - 2026, Juicy, Inc.
+
+#pragma once
+
+namespace Debug {
+static void Print(const FString& Msg,
+                  const FColor& Color = FColor::MakeRandomColor(),
+                  int32 InKey = -1)
+{
+  if (GEngine)
+  {
+    GEngine->AddOnScreenDebugMessage(InKey, 7.f, Color, Msg);
+
+    UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg);
+  }
+}
+}  // namespace Debug
