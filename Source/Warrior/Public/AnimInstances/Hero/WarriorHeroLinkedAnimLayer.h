b70945cacb9f571a8cc3a84bcf9d80fd8cc38ec1
juice-artur <asasin.lol10@gmail.com>
2026-01-10 18:49:38 +0200

    Initial implementation anim layer

--- Source/Warrior/Public/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.h
+++ Source/Warrior/Public/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.h
@@ -2,14 +2,17 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "AnimInstances/WarriorBaseAnimInstance.h"
+#include "CoreMinimal.h"
 #include "WarriorHeroLinkedAnimLayer.generated.h"
 
-
+class UWarriorHeroAnimInstance;
 UCLASS()
 class WARRIOR_API UWarriorHeroLinkedAnimLayer : public UWarriorBaseAnimInstance
 {
 	GENERATED_BODY()
 
+public:
+    UFUNCTION(BlueprintPure, meta = (BlueprintThreadSafe))
+    UWarriorHeroAnimInstance* GetHeroAnimInstance() const;
 };
