c3727ecb0b7e758e1b4a0c38171f77a621027cd5
juice-artur <asasin.lol10@gmail.com>
2026-03-06 19:41:08 +0200

    Configure AI avoidance

--- Source/Warrior/Public/Controllers/WarriorAIController.h
+++ Source/Warrior/Public/Controllers/WarriorAIController.h
@@ -33,4 +33,16 @@ protected:
 protected:
     UFUNCTION()
     virtual void OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+
+    virtual void BeginPlay() override;
+
+private:
+    UPROPERTY(EditDefaultsOnly, Category = "Detour Crowd Avoidance Config")
+    bool bEnableDetourCrowdAvoidance = true;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Detour Crowd Avoidance Config", meta = (EditCondition = "bEnableDetourCrowdAvoidance", UIMin = "1", UIMax = "4"))
+    int32 DetourCrowdAvoidanceQuality = 4;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Detour Crowd Avoidance Config", meta = (EditCondition = "bEnableDetourCrowdAvoidance"))
+    float CollisionQueryRange = 600.f;
 };
