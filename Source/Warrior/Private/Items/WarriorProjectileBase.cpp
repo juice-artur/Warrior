8c2685bab987ae257c19032327296fc14a6967a8
juice-artur <asasin.lol10@gmail.com>
2026-03-29 19:03:36 +0300

    Create ability for enemy to shoot projectile

---
+++ Source/Warrior/Private/Items/WarriorProjectileBase.cpp
@@ -0,0 +1,65 @@
+// Warrior, Copyright 2026 - 2026, Juicy, Inc.
+
+#include "Items/WarriorProjectileBase.h"
+#include "Components/BoxComponent.h"
+#include "NiagaraComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+#include "WarriorDebugHelper.h"
+
+
+AWarriorProjectileBase::AWarriorProjectileBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+    ProjectileCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("ProjectileCollisionBox"));
+    SetRootComponent(ProjectileCollisionBox);
+    ProjectileCollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    ProjectileCollisionBox->SetCollisionResponseToChannel(ECC_Pawn,ECR_Block);
+    ProjectileCollisionBox->SetCollisionResponseToChannel(ECC_WorldDynamic,ECR_Block);
+    ProjectileCollisionBox->SetCollisionResponseToChannel(ECC_WorldStatic,ECR_Block);
+    ProjectileCollisionBox->OnComponentHit.AddUniqueDynamic(this, &ThisClass::OnProjectileHit);
+    ProjectileCollisionBox->OnComponentBeginOverlap.AddUniqueDynamic(this, &ThisClass::OnProjectileBeginOverlap);
+
+    ProjectileNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("ProjectileNiagaraComponent"));
+    ProjectileNiagaraComponent->SetupAttachment(GetRootComponent());
+
+    ProjectileMovementComp = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComp"));
+    ProjectileMovementComp->InitialSpeed = 700.f;
+    ProjectileMovementComp->MaxSpeed = 900.f;
+    ProjectileMovementComp->Velocity = FVector(1.f,0.f,0.f);
+    ProjectileMovementComp->ProjectileGravityScale = 0.f;
+
+    InitialLifeSpan = 4.f;
+}
+
+void AWarriorProjectileBase::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (ProjectileDamagePolicy == EProjectileDamagePolicy::OnBeginOverlap)
+    {
+        ProjectileCollisionBox->SetCollisionResponseToChannel(ECC_Pawn,ECR_Overlap);
+    }
+}
+
+void AWarriorProjectileBase::OnProjectileHit(UPrimitiveComponent* HitComponent,
+                                             AActor* OtherActor,
+                                             UPrimitiveComponent* OtherComp,
+                                             FVector NormalImpulse,
+                                             const FHitResult& Hit)
+{
+    if (OtherActor)
+    {
+        Debug::Print(OtherActor->GetActorNameOrLabel());
+        Destroy();
+    }
+}
+
+void AWarriorProjectileBase::OnProjectileBeginOverlap(
+    UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+    const FHitResult& SweepResult)
+{
+
+}
---
+++ Source/Warrior/Public/Items/WarriorProjectileBase.h
@@ -0,0 +1,52 @@
+// Warrior, Copyright 2026 - 2026, Juicy, Inc.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WarriorProjectileBase.generated.h"
+
+class UBoxComponent;
+class UNiagaraComponent;
+class UProjectileMovementComponent;
+
+UENUM(BlueprintType)
+enum class EProjectileDamagePolicy : uint8
+{
+    OnHit,
+    OnBeginOverlap
+};
+
+
+UCLASS()
+class WARRIOR_API AWarriorProjectileBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWarriorProjectileBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+    UFUNCTION()
+    virtual void OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+        FVector NormalImpulse, const FHitResult& Hit);
+
+    UFUNCTION()
+    virtual void OnProjectileBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+protected:
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+    UBoxComponent* ProjectileCollisionBox;
+
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+    UNiagaraComponent* ProjectileNiagaraComponent;
+
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+    UProjectileMovementComponent* ProjectileMovementComp;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+    EProjectileDamagePolicy ProjectileDamagePolicy = EProjectileDamagePolicy::OnHit;
+};
