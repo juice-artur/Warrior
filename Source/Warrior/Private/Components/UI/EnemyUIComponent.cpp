d463e65fd8b4372d7c5f41c2d0b1f9b47232ace1
juice-artur <asasin.lol10@gmail.com>
2026-03-31 20:26:24 +0300

    Remove widget when giant died

--- Source/Warrior/Private/Components/UI/EnemyUIComponent.cpp
+++ Source/Warrior/Private/Components/UI/EnemyUIComponent.cpp
@@ -1,4 +1,27 @@
 // Warrior, Copyright 2026 - 2026, Juicy, Inc.
 
 #include "Components/UI/EnemyUIComponent.h"
+#include "Widgets/WarriorWidgetBase.h"
 
+
+void UEnemyUIComponent::RegisterEnemyDrawnWidget(UWarriorWidgetBase* InWidgetToRegister)
+{
+    EnemyDrawnWidgets.Add(InWidgetToRegister);
+}
+
+void UEnemyUIComponent::RemoveEnemyDrawnWidgetsIfAny()
+{
+
+    if (EnemyDrawnWidgets.IsEmpty())
+    {
+        return;
+    }
+
+    for (UWarriorWidgetBase* DrawnWidget : EnemyDrawnWidgets)
+    {
+        if (DrawnWidget)
+        {
+            DrawnWidget->RemoveFromParent();
+        }
+    }
+}
--- Source/Warrior/Public/Components/UI/EnemyUIComponent.h
+++ Source/Warrior/Public/Components/UI/EnemyUIComponent.h
@@ -6,10 +6,21 @@
 #include "Components/UI/PawnUIComponent.h"
 #include "EnemyUIComponent.generated.h"
 
+class UWarriorWidgetBase;
+
 
 UCLASS()
 class WARRIOR_API UEnemyUIComponent : public UPawnUIComponent
 {
 	GENERATED_BODY()
 
+public:
+	UFUNCTION(BlueprintCallable)
+	void RegisterEnemyDrawnWidget(UWarriorWidgetBase* InWidgetToRegister);
+
+	UFUNCTION(BlueprintCallable)
+	void RemoveEnemyDrawnWidgetsIfAny();
+
+private:
+	TArray<UWarriorWidgetBase*> EnemyDrawnWidgets;
 };
